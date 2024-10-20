7bf115c7b11c45a93c3fc4af54661a24919081b4
Inna <ogsingerlan13@gmail.com>
2024-10-20 16:23:46 +0300

    Update La.c
    Added additional part in the function to check for the last sequence of characters in str

--- Upr3/La.c
+++ Upr3/La.c
@@ -1,5 +1,5 @@
 
-// Write a function that takes a string -> words, separated by one interval each
+// Write a function that takes a string -> words, separated by one interval each,
 // the fuction must return the address of the first word, which ends in "la"
 
 #include <stdio.h>
@@ -9,9 +9,9 @@ char* ends_with(char*, char*, int);
 
 int main() {
 
-    char string[N] = "mol berry kola cherry bang\0";
+    char string[N] = "berry cherry bang kot kola\0";
     
-    printf("address %c : %p\n", *(string + 10), (string + 10));
+    printf("address of letter %c: %p\n", *(string + 22), string + 22);
     char* address = ends_with(string, "la\0", 2);
     printf("address: %p\n", address);
     return 0;
@@ -20,39 +20,55 @@ int main() {
 char* ends_with(char* str, char* suffix, int size_suffix) {
 
     char* address_check = NULL;
-    // i -> iterates through str
-    // j_suffix -> iterates through str_check
-    // count_characters -> keeps the length of last found sequence of characters in str
-    int i = 0, j_suffix, count_characters = 1;
+    // j_suffix -> iterates through suffix
+    // count_characters -> keeps the length of the 
+    // last found sequence of characters in str
+    int j_suffix, count_characters = 0;
 
-    while(*(str + i)) {
+    while(*str) {
 
         // no need to further check, if 
         // size_suffix >= count_characters
         // <ch> can't contain <cherry>
-        if(*(str + i) == ' ' && size_suffix <= count_characters) {
+        if(*str == ' ' && size_suffix <= count_characters) {
 
-            // because we don't need to count the ' ' charachter
-            count_characters--;
-            address_check = str + i - count_characters;
             j_suffix = 0;
 
-            while(*(str + i - size_suffix + j_suffix) == *(suffix + j_suffix) 
-                        && *(suffix))
+            while(*(str - size_suffix + j_suffix) == *(suffix + j_suffix) 
+                        && *(suffix + j_suffix))
                 j_suffix ++;
             
             // all characters match
             if(j_suffix == size_suffix){
                 
+                address_check = str - count_characters;
                 printf("Starting character: %c\n", *address_check);
                 return address_check;
             }
             
             count_characters = 0;
         }
+        else
+            count_characters ++;
 
-        i++;
-        count_characters ++;
+        str ++;
+    }
+
+    // because the last sequence of characters is skipped in while loop,
+    // perform additional check
+    if(size_suffix <= count_characters) {
+
+        j_suffix = 0;
+        while(*(str - size_suffix + j_suffix) == *(suffix + j_suffix) &&
+                *(suffix + j_suffix))
+            j_suffix++;
+
+        if(j_suffix == size_suffix){
+            
+            address_check = str - count_characters;
+            printf("Starting character: %c\n", *address_check);
+                return address_check;
+        }
     }
 
     return address_check;
